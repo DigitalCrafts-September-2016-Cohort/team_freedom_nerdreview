906cd11c6db1c0593ce668864f95dba961b11fb7
John Coppola <jdcoppola@gmail.com>
2016-10-18 15:53:54 -0400

    updates database and nesting up through third level

--- server.py
+++ server.py
@@ -27,9 +27,9 @@ def render_sub_cats(cat_id):
        result_list = query.namedresult()
    )
 
-@app.route('/categories/<cat_id>/<sub_cat_id>')
+@app.route('/categories/<sub_cat_id>/products')
 def render_sub_cat_products(sub_cat_id):
-    query = db.query('select * from products where products.secondary_cat_id = %s' % sub_cat_id)
+    query = db.query('select * from product_uses_category where product_uses_category.secondary_cat_id = %s' % sub_cat_id)
     return render_template(
         '/sub_categories_products.html',
         result_list = query.namedresult()
