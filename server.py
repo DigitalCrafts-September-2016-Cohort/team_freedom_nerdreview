67fe00bcc222632bb283df13f9662c8a85f39e21
Jesslyn Landgren <jesslyn.landgren@gmail.com>
2016-10-19 10:55:58 -0400

    Added 2 basic tile elements and queries

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def render_categories():
 def render_sub_cats(cat_id):
    # Reduce reduncancy by joining tables and being more specific in our select
    cat_query = db.query('select * from main_cat')
-   sub_cat_query = db.query('select * from secondary_cat where secondary_cat.main_cat_id = %s' % cat_id)
+   sub_cat_query = db.query('select product.name as prod_name, avg(review.rating) as avg_rating, count(review.product_id) as review_count from review inner join product on product.id = review.product_id inner join secondary_cat on secondary_cat.id = %s group by product.name' % cat_id)
    return render_template(
       '/sub_categories.html',
       cat_id = cat_id,
@@ -36,6 +36,9 @@ def render_sub_cat_products(cat_id, sub_cat_id):
    # Reduce reduncancy by joining tables and being more specific in our select
    sub_cat_query = db.query('select * from secondary_cat where secondary_cat.main_cat_id = %s' % cat_id)
    sub_cat_products_query = db.query('select product.name, product_uses_category.id from product_uses_category inner join product on product.id = product_uses_category.product_id where product_uses_category.secondary_cat_id = %s' % sub_cat_id)
+
+   sub_cat_query = db.query('select product.name as prod_name, avg(review.rating) as avg_rating, count(review.product_id) as review_count from review inner join product on product.id = review.product_id inner join secondary_cat on secondary_cat.id = %s group by product.name' % cat_id)
+   
    return render_template(
       '/sub_categories_products.html',
       cat_id = cat_id,
@@ -47,7 +50,7 @@ def render_sub_cat_products(cat_id, sub_cat_id):
 @app.route('/reviews')
 def render_reviews():
    # We can extract the rest of the data we need in the query later
-   query = db.query('select * from review')
+   query = db.query('select product.name as prod_name, review.rating, users.name as user_name, review.id from review, product, users where review.product_id = product.id and review.user_id = users.id')
    return render_template(
       '/reviews.html',
       reviews_list = query.namedresult()
@@ -55,8 +58,8 @@ def render_reviews():
 
 @app.route('/reviews/<review_id>')
 def render_individual_review(review_id):
-   review_query = db.query('select * from review where id = %s' % review_id)
-   return render_template(
+    review_query = db.query("select product.name as prod_name, review.rating, review.date, users.name as user_name, review.id, review.review from review, product, users where review.product_id = product.id and review.user_id = users.id and review.id = '%s'" % review_id)
+    return render_template(
       '/individual_review.html',
       review = review_query.namedresult()[0]
    )
