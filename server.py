633c223e0b1a2098d183010b2987ca9523b276b5
Robert Dunn <robertdunn220@gmail.com>
2016-10-19 20:07:56 -0400

    Rounding function for average numbers

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ def render_categories():
 def render_sub_cats(cat_id):
     # Reduce reduncancy by joining tables and being more specific in our select
     cat_query = db.query('select * from secondary_cat where secondary_cat.main_cat_id = %s' % cat_id)
-    sub_cat_query = db.query('select product.name as prod_name, product.id as prod_id, avg(review.rating) as avg_rating,  count(review.product_id) as review_count from review inner join product on product.id = review.product_id inner join product_uses_category on product.id = product_uses_category.product_id inner join secondary_cat on product_uses_category.secondary_cat_id = secondary_cat.id where secondary_cat.main_cat_id = %s group by product.name, prod_id' % cat_id)
+    sub_cat_query = db.query('select product.name as prod_name, product.id as prod_id, round(avg(review.rating), 2) as avg_rating,  count(review.product_id) as review_count from review inner join product on product.id = review.product_id inner join product_uses_category on product.id = product_uses_category.product_id inner join secondary_cat on product_uses_category.secondary_cat_id = secondary_cat.id where secondary_cat.main_cat_id = %s group by product.name, prod_id' % cat_id)
     return render_template(
         '/sub_categories.html',
         cat_id = cat_id,
@@ -39,7 +39,7 @@ def render_sub_cat_products(cat_id, sub_cat_id):
 
     #Gets all products in the secondary category with id = sub_cat_id
     ## THIS IS THE RIGHT ONE
-    sub_cat_products_query = db.query('select product.name as prod_name, product.id as prod_id, avg(review.rating) as avg_rating, count(review.product_id) as review_count from review inner join product on product.id = review.product_id inner join product_uses_category on product.id = product_uses_category.product_id inner join secondary_cat on product_uses_category.secondary_cat_id = secondary_cat.id where secondary_cat.id = %s group by product.name, product.id order by prod_name' % sub_cat_id)
+    sub_cat_products_query = db.query('select product.name as prod_name, product.id as prod_id, round(avg(review.rating), 2) as avg_rating, count(review.product_id) as review_count from review inner join product on product.id = review.product_id inner join product_uses_category on product.id = product_uses_category.product_id inner join secondary_cat on product_uses_category.secondary_cat_id = secondary_cat.id where secondary_cat.id = %s group by product.name, product.id order by prod_name' % sub_cat_id)
 
 
     return render_template(
@@ -63,7 +63,7 @@ def disp_individual_product(product_id):
     product_query = db.query('select * from product where product.id = %s' % product_id)
 
     #Gets the summary stats (count, avg rating) for all the product (see above) reviews
-    product_reviews_summary_query = db.query('select count(review.id) as review_count, avg(review.rating) as avg_rating from product inner join review on review.product_id = product.id and product.id = %s' % product_id)
+    product_reviews_summary_query = db.query('select count(review.id) as review_count, round(avg(review.rating), 2) as avg_rating from product inner join review on review.product_id = product.id and product.id = %s' % product_id)
 
     #Gets all the reviews for the indiviudal product from above
     reviews_query = db.query('select review.id as review_id from product inner join review on product.id = review.product_id and product.id = %s' % product_id)
