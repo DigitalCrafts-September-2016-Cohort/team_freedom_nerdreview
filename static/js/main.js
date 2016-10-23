c60a1ef4e8815032df5511de3cbe509016d7a24c
John Coppola <jdcoppola@gmail.com>
2016-10-22 20:05:46 -0400

    adds client-side verification to the add new review page

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,33 @@
-// // Back button goes back one page on click
-// // Doesn't work perfectly: if you come from a different website, it'll take you back there instead of down a level
-$('.js-back').on('click', function(){
-    history.go(-1);
-    return false;
+$(document).ready(function(){
+    // BACK BUTTON
+    // Back button goes back one page on click
+    // Doesn't work perfectly: if you come from a different website, it'll take you back there instead of down a level
+    $('.js-back').on('click', function(){
+        history.go(-1);
+        return false;
+    });
+
+    // ADD NEW REViEW
+    // Loops through the input/select tags and enables the Submit button when all fields are filled out
+    (function() {
+        $('.inputs input, select').keyup(function() {
+            var empty = false;
+            $('.inputs input, select').each(function() {
+                if ($(this).val() == '') {
+                    empty = true;
+                }
+            });
+            if (empty) {
+                $('.js-submit').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+            } else {
+                $('.js-submit').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+            }
+        });
+    })()
+
+    // INDIVIDUAL CONTENT
+    // On individual content pages, set the height of the tiles in the main/header and main/body to be even
+    // Script stops running if .img-list isn't on the page, so keep this part at bottom
+    var imgListTop = $('.img-list').position().top;
+    $('.content').css('top', imgListTop - 68);
 });
-
-// On individual content pages, set the height of the tiles in the main/header and main/body to be even
-
-// console.log('imgList top: ' + $('.img-list').position().top);
-// console.log('body top: ' + $('.content').position().top);
-
-var imgListTop = $('.img-list').position().top;
-// console.log('imgList top var: ' + imgListTop);
-$('.content').css('top', imgListTop - 68);
-
-// console.log('body top: ' + $('.content').position().top);
