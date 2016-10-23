2147bb602d1b171da35f98aec04d67838d844865
John Coppola <jdcoppola@gmail.com>
2016-10-23 16:00:23 -0400

    major overhaul to responsive styles (individual page), adds more site navigation

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     // SLIDE NAVBAR MENU
     $('.js-slide').on('click', function(){
         if ($('.nav-menu').position().left === 0) {
@@ -8,12 +9,33 @@ $(document).ready(function(){
         }
     });
 
+    $(window).resize(function() {
+        if ($(window).width() > 1000) {
+            $('.nav-menu').css('left', '0');
+        }
+    });
+
     // BACK BUTTON
     // Back button goes back one page on click
     // Doesn't work perfectly: if you come from a different website, it'll take you back there instead of down a level
     $('.js-back').on('click', function(){
         history.go(-1);
         return false;
+    });
+
+    // SEE IMAGES BUTTON
+    // Show/hide images on click for small screens to save space
+    $('.js-images-btn').on('click', function(){
+        $('.img-list > ul').slideToggle();
+        // var btnText = $('.js-images-btn > h4').html();
+        // if ($('.img-list > ul').is(':visible')) {
+        //     $('.img-list > ul').css('display', 'inline-block');
+        // }
+        if ($('.js-images-btn > h4').html() === 'Show images') {
+            $('.js-images-btn > h4').html('Hide images');
+        } else {
+            $('.js-images-btn > h4').html('Show images');
+        }
     });
 
     // ADD NEW REViEW
@@ -33,41 +55,5 @@ $(document).ready(function(){
             }
         });
     })()
-
-
-    var mainWrapperWidth = $('.main-wrapper').width();
-    var imgListTop = $('.ind-header > .img-list').position().top;
-
-
-    if ($(window).width() > 1000) {
-        $('.content').css('top', imgListTop - 98);
-        $('.ind-body').width(mainWrapperWidth - 300);
-    }
-
-    $(window).resize(function() {
-        if ($(window).width() > 1000) {
-            $('.ind-body').removeClass('width100');
-            imgListTop = $('.ind-header > .img-list').position().top;
-            $('.content').css('top', imgListTop - 98);
-            $('.ind-body').width(mainWrapperWidth - 300);
-            $('.nav-menu').css('left', '0');
-        }
-        else {
-            $('.ind-body').addClass('width100');
-        }
-        var mainWrapperWidth = $('.main-wrapper').width();
-        $('.ind-body').width(mainWrapperWidth - 300);
-
-
-    });
-
-    // = $('.ind-body').width() - 250;
-
-    // INDIVIDUAL CONTENT
-    // On individual content pages, set the height of the tiles in the main/header and main/body to be even
-    // Script stops running if .img-list isn't on the page, so keep this part at bottom
-    // var imgListTop = $('.ind-header > .img-list').position().top;
-    // $('.content').css('top', imgListTop - 98);
-
 
 });
